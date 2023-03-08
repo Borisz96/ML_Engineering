abd97e691274a2135196c1451812414de1169711
Miklos Kosarszky <miklos_kosarszky@epam.com>
2023-03-08 15:09:27 +0100

    adding source code

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,10 +1,17 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 import sys
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+# input_file = sys.argv[1]
+# output_file = sys.argv[2]
 
-df = pd.read_csv(input_file)
-# perform preprocessing
-preprocessed_df = ...
-preprocessed_df.to_csv(output_file, index=False)
+def read_data(path):
+    df = pd.read_csv(path)
+    # Split the data
+    X = df.drop('class', axis=1)
+    y = df['class']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    return X_train, X_test, y_train, y_test
+
--- src/train.py
+++ src/train.py
@@ -1,17 +1,45 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.metrics import accuracy_score
 import pickle
+import dvc.api
 import sys
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+# input_file = sys.argv[1]
+# output_file = sys.argv[2]
 
-df = pd.read_csv(input_file)
-X = df.drop('target', axis=1)
-y = df['target']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-model = LogisticRegression()
-model.fit(X_train, y_train)
-with open(output_file, 'wb') as f:
-    pickle.dump(model, f)
+
+def train_and_evaluate_models(X_train, X_test, y_train, y_test):
+    # Define the list of classifiers to evaluate
+    classifiers = [LogisticRegression(), DecisionTreeClassifier(), RandomForestClassifier(), KNeighborsClassifier(),
+                   GradientBoostingClassifier()]
+
+    # Create an empty dictionary to store the scores for each classifier
+    scores = {}
+
+    # Loop through each classifier and train it on the training data, then test it on the testing data and store the score
+    for clf in classifiers:
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        score = accuracy_score(y_test, y_pred)
+        scores[clf.__class__.__name__] = score
+
+    # Find the classifier with the highest score
+    best_clf = max(scores, key=scores.get)
+
+    # Print the results
+    print("Best classifier:", best_clf)
+    print("Accuracy score:", scores[best_clf])
+    best_accuracy = scores[best_clf]
+
+    # Log the best model and its accuracy
+    with dvc.api.open('best_model.pkl', 'wb') as file:
+        pickle.dump(best_clf, file)
+
+    with dvc.api.open('best_accuracy.txt', 'w') as file:
+        file.write(str(best_accuracy))
+
+    return best_clf, best_accuracy
